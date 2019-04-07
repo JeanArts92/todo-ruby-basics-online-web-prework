a8710915af5815c11af174030b20dd473ddc04be
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-07 11:00:15 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
-  12 / 2 
+  num1 = 18
+  num2 = 3
+  total = 18 / 3
  end
 
 def assign_variable(name = "Bob")
-  puts ({#name})
+  name = "Bob"
+  
 end
 
-def argue(name="Royce")
-  puts ("Hello #{name}")
+def argue(phrase = "I'm right and you are wrong!")
+  return "#{phrase}"
 end
 
-def greeting(name, nationality)
-  puts "Hi #{name} who is of #{nationality} descent"
+def pizza_party(x = "cheese")
+  return "#{x}"
 end
 
 def return_a_value
+  return "Nice"
 
 end
 
 def last_evaluated_value
+  return "expert"
 
 end
 
-def pizza_party
-
+def greeting(name, language)
+  puts "Hi #{name}, it looks like you're learning #{language}"
 end
+
